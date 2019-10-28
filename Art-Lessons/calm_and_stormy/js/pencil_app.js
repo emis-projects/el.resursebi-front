cd9006c82df2301f33aebdd2c88d872c5bd7d717
Rome00 <45652495+Rome00@users.noreply.github.com>
2019-10-28 18:39:59 +0400

    1366-157(157, 209-213), 91, 92, 78, 208

---
+++ Art-Lessons/calm_and_stormy/js/pencil_app.js
@@ -0,0 +1,28 @@
+const lines = document.querySelectorAll('.img_box-line img');
+const penciles = document.querySelectorAll('.img_box');
+
+
+for (var i = 0; i < penciles.length; i++) {
+    penciles[i].addEventListener('click', ((j) => {
+        return function () {
+            penciles.forEach(el => {
+                el.style.opacity = "0.5";
+            });
+            lines.forEach(e => {
+                e.style.opacity = "0";
+            });
+            penciles[j].style.opacity = "1";
+            lines[j].style.opacity = "1";
+            lines[j].classList.add('fade-in');
+        };
+    })(i));
+}
+
+
+
+
+/// opacity-5
+
+/// opacity-1
+
+/// opacity-0
