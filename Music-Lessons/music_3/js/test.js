8c304449d2d06527eef2af6c1921ebebfccadad8
Rome00 <45652495+Rome00@users.noreply.github.com>
2019-11-11 16:12:33 +0400

    M-1633-22-26

--- Music-Lessons/music_3/js/test.js
+++ Music-Lessons/music_3/js/test.js
@@ -1,4 +1,17 @@
-console.log(x);
-var x = 5;
-x += 5;
-console.log(x);
+const dots = document.querySelectorAll('.dot-select span');
+
+
+for (let i = 0; i < dots.length; i++) {
+    dots[i].addEventListener('click', (el) => {
+        const target = el.target
+        if (target.classList.contains('active')) {
+            target.classList.remove('active')
+        } else {
+            dots.forEach(e => {
+                e.classList.remove('active')
+            })
+            target.classList.add('active')
+        }
+    })
+
+}
