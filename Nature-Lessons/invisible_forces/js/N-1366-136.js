ee3aca388ced2bcd3327acf643a3ce11a8a63310
Rome00 <r.khazalia@gmail.com>
2019-12-17 21:59:54 +0400

    136 game nature #2

---
+++ Nature-Lessons/invisible_forces/js/N-1366-136.js
@@ -0,0 +1,38 @@
+const dots = document.querySelectorAll('.dot-select span');
+const reset = document.getElementById('resetBtn');
+const check = document.getElementById('completedGame');
+
+// მონიშვნა
+for (let i = 0; i < dots.length; i++) {
+	dots[i].addEventListener('click', evt => {
+		init();
+		evt.target.classList.add('selected');
+	});
+}
+
+// შემოწმება სისწორეზე
+check.addEventListener('click', evt => {
+	for (let i = 0; i < dots.length; i++) {
+		if (dots[i].classList.contains('selected') && dots[i].dataset.check === 'true') {
+			dots[i].classList.remove('selected');
+			dots[i].classList.add('success');
+			window.location.href = './N-1366-117-success.html';
+		} else if (dots[i].classList.contains('selected')) {
+			dots[i].classList.remove('selected');
+			dots[i].classList.add('error');
+		}
+	}
+	console.log(dots[i]);
+
+	evt.target.setAttribute('disabled', true);
+});
+
+reset.addEventListener('click', init);
+
+// გასუფთავება
+function init() {
+	dots.forEach(elm => {
+		elm.classList.remove('selected');
+		elm.classList.remove('error');
+	});
+}
