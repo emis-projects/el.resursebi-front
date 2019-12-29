81ffe29f894d3222e41903b2c8420b6c4c6b7f57
Rome00 <r.khazalia@gmail.com>
2019-12-29 14:47:44 +0400

    country game finish

--- Nature-Lessons/Summer-in-winter/js/N-1366-06-52.js
+++ Nature-Lessons/Summer-in-winter/js/N-1366-06-52.js
@@ -1,7 +1,14 @@
+// variables for data store
 const countryDrop = [];
 const countryDrag = [];
 
-$(document).ready(function () {
+let dropItemParent = [];
+let dropItem = [];
+
+
+//init func on load
+$(document).ready(() => {
+    //init drag
     $(".draggable").draggable({
         revert: "invalid",
         //clear transition if exist
@@ -10,46 +17,84 @@ $(document).ready(function () {
                 left: '',
                 top: ''
             })
+        },
+        start: function (event, ui) {
+            dropItemParent.push(ui.helper.parent());
         }
     });
-
+    //init drop
     $(".dropable").droppable({
         // center dropped image
         drop: function (event, ui) {
+            //ondrop append element  & reset left/top
             $(this).children('div').append(ui.draggable.css({
                 left: '',
                 top: ''
             }));
+            //get dropped & dragged items data attr
             let drop = $(this).children('div').data().country;
             let drag = ui.draggable.data().country;
+            // save data attrs in arr
             countryDrag.push(drag);
             countryDrop.push(drop);
-            ui.draggable.draggable('destroy');
+            // save dragged elems
+            dropItem.push(ui.draggable);
+            //after drop destroy drag fn
+            ui.draggable.draggable('disable');
         }
     });
 });
 
+
+// call init and fin fn on click
 document.getElementById('completedGame').addEventListener('click', finFn);
+document.getElementById('resetBtn').addEventListener('click', initFn);
 
-
-function finFn() {
-    console.log(countryDrop)
+// check function
+function finFn(evt) {
+    let countryBox = $('.country_box');
+    //if equal success
     if (arraysEqual(countryDrop, countryDrag) && countryDrag.length === 12) {
         window.location.href = 'N-1366-06-47-success.html'
     }
-    /*let difArr = [];
-    for (let i = 0; i < countryDrop.length; i++) {
-        if (countryDrop[i] !== countryDrag[i]) {
-            difArr.push(countryDrag[i]);
+    // change all dropped items bg #dc6c85
+    countryBox.children().css({
+        background: "#dc6c85",
+        color: '#fff'
+    });
+    // make correct items bg white
+    for (let i = 0; i < dropItem.length; i++) {
+        let dataItem = dropItem[i].data('country');
+        let parentItem = dropItem[i].parent().data('country');
+        if (dataItem === parentItem) {
+            dropItem[i].css({
+                background: '#fff',
+                color: '#262626'
+            })
         }
-    }*/
-    for (let i = 0; i < countryDrag.length; i++) {
-        let dataItem = $('.dropable').children().children('[data-country="' + countryDrag[i] + '"]');
-        console.log(dataItem.data('country') + '------⤵')
-        console.log(dataItem.parent().data('country'))
+    }
+    if (countryBox.children().length > 0) {
+        evt.target.setAttribute('disabled', 'disabled');
     }
 }
 
+function initFn() {
+    $('.country_box').children().css({
+        background: '',
+        color: ''
+    });
+    for (let i = 0; i < dropItemParent.length; i++) {
+        dropItem[i].css({
+            background: ''
+        });
+        dropItemParent[i].append(dropItem[i])
+    }
+    document.getElementById('completedGame').removeAttribute('disabled');
+    //enable drag
+    $('.draggable').draggable('enable')
+}
+
+// check for array equality
 function arraysEqual(arr1, arr2) {
     for (let i = arr1.length; i--;) {
         if (arr1[i] !== arr2[i])
