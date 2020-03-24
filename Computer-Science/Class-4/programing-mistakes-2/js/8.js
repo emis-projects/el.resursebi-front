caccc16c348d8893d6d3c982252e08207d3d3138
Shota Zenaishvili <shotazenaishvili@gmail.com>
2020-03-24 20:22:38 +0400

    Cs-class4-pm-2/8(game)

---
+++ Computer-Science/Class-4/programing-mistakes-2/js/8.js
@@ -0,0 +1,63 @@
+function computerGames(){
+    var checkmark = document.querySelectorAll('.checkmark');
+
+    var completedBtn = document.getElementById('completedGame');
+    var resetBtn = document.getElementById('resetBtn');
+    $(checkmark).on('click', (e) => this.clickMe(e));
+
+
+    this.clickMe = (e) => {
+        e.target.classList.add('selectedPurple');
+        
+    }
+
+    this.completGame = () => {
+        var count = 0;
+        var isError = false;
+        checkmark.forEach(element => {
+            if(element.classList.contains('selectedPurple')){
+                if(element.classList.contains('correct')){
+                    count++;
+                }
+                if((element.classList.contains('noCorrect'))){
+                    isError = true;
+                    $(element).removeClass( "selectedPurple")
+                    element.classList.add('error');
+                }
+            }
+        });
+        
+
+        if (count == 2 && !isError) {
+            this.successPage();
+        }
+
+
+        completedBtn.setAttribute('disabled', 'true');
+    }
+
+
+
+    this.init = (e) =>{
+        checkmark.forEach(element => {
+            $(element).removeClass( "error")
+            $(element).removeClass( "selectedPurple")
+            element.style = '';
+        });
+        completedBtn.removeAttribute('disabled');
+    }
+
+    this.successPage = () => {
+        location.href = 'game-success-8.html';
+    }
+
+
+    resetBtn.addEventListener('click', () => this.init());
+    completedBtn.addEventListener('click', () => this.completGame());
+}
+
+
+
+
+
+const computergame = new computerGames();
