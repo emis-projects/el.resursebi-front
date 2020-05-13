dd268088b6bf11dae1d7487475ef31f104d82474
Shota Zenaishvili <shotazenaishvili@gmail.com>
2020-05-13 17:41:48 +0400

    Cs-class4-5/6

---
+++ Computer-Science/Class-4/UI-5/js/6.js
@@ -0,0 +1,72 @@
+function computerGames(){
+    var checkmark = document.querySelectorAll('.checkmark');
+
+    var completedBtn = document.getElementById('completedGame');
+    var resetBtn = document.getElementById('resetBtn');
+    $(checkmark).on('click', (e) => this.clickMe(e));
+
+
+    this.clickMe = (e) => {
+        // checkmark.forEach(element => {
+        //     $(element).removeClass( "error")
+        //     $(element).removeClass( "active")
+        //     element.style = '';
+        // });
+        e.target.classList.add('active');
+    }
+
+    this.completGame = () => {
+        var count = 0;
+        var isError = false;
+        checkmark.forEach(element => {
+            if(element.classList.contains('active')){
+                if(element.classList.contains('correct')){
+                    $(element).removeClass( "active")
+                    element.classList.add('success');
+                    count++;
+                }
+            }
+            if((element.classList.contains('active'))){
+                if((element.classList.contains('noCorrect'))){
+                    isError = true;
+                    $(element).removeClass( "active")
+                    element.classList.add('error');
+                }
+            }
+        });
+        
+
+        if (count == 4 && !isError) {
+            this.successPage();
+        }
+
+
+        completedBtn.setAttribute('disabled', 'true');
+    }
+
+
+
+    this.init = (e) =>{
+        checkmark.forEach(element => {
+            $(element).removeClass( "error")
+            $(element).removeClass( "success")
+            $(element).removeClass( "active")
+            //element.style = '';
+        });
+        completedBtn.removeAttribute('disabled');
+    }
+
+    this.successPage = () => {
+        location.href = 'game-success-6.html';
+    }
+
+
+    resetBtn.addEventListener('click', () => this.init());
+    completedBtn.addEventListener('click', () => this.completGame());
+}
+
+
+
+
+
+const computergame = new computerGames();
