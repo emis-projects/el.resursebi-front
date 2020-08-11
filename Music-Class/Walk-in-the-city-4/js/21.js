3b668c3e4757de6315cfd49bab315a692e2f3758
Rome00 <r.khazalia@gmail.com>
2020-08-11 11:25:17 +0400

    game 21

--- Music-Class/Walk-in-the-city-4/js/21.js
+++ Music-Class/Walk-in-the-city-4/js/21.js
@@ -1,122 +1,78 @@
-const listen = document.getElementById("listen-btn");
-const secondLine = document.getElementById('second-half')
-const firstBox = document.querySelectorAll("#box-1 img");
-const secondBox = document.querySelectorAll("#box-2 img");
-const thirdBox = document.querySelectorAll("#box-3 img");
-const checkBoxes = document.querySelectorAll('.check-box')
+const cards = document.querySelectorAll('.memory-card');
 
-let isActive = false;
-let firstStage = true;
-let voiceId = 1;
-let secondCheck = null;
-let check = [];
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
 
 
-createjs.Sound.on("fileload", handleLoadComplete);
-createjs.Sound.alternateExtensions = ["wav"];
+// გადატრიალება
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
 
-function handleLoadComplete(event) {
-  createjs.Sound.play("sound");
+    this.classList.add('flip');
+
+    if (!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+    secondCard = this;
+
+    checkForMatch();
 }
 
-function handleLoadstop(event) {
-  createjs.Sound.stop("sound");
+// შემოწმება ემთხვევა თუ არა პირველი და მეორე გადატრიალება ერთმანეთს
+function checkForMatch() {
+    let isMatch = firstCard.dataset.slide === secondCard.dataset.slide
+    // თუ დაემთხვა მოვხსნათ დაკლიკვა, თუ არა გადავატრიალოთ
+    isMatch ? disableCards() : unFlipCards();
+
 }
 
-listen.addEventListener("click", (evt) => {
-  handleLoadstop();
-  createjs.Sound.registerSound({
-    src: `${evt.target.getAttribute("data-voice")}`,
-    id: "sound",
-  });
-  handleLoadComplete();
-  voiceId = 1;
-  isActive = true
-});
+// მოვხსნათ დაკლიკვა
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard)
+    secondCard.removeEventListener('click', flipCard)
 
-firstBox.forEach((el) => {
-  // ეშვება დაკვრის ფუნქცია
-  play(el)
-});
+    resetBoard();
+}
+// გადავატრიალოთ 1 წმ შემდეგ
+function unFlipCards() {
+    //დავბლოკოთ მესამე, თუ 1 წმ
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip')
+        secondCard.classList.remove('flip')
 
+        resetBoard();
+    }, 1000)
+}
+// დავაბრუნოთ დავდაპირველ მდგომარეობაზე 1 და მეორე მოვხსნათ დაბლოკვა
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
 
-secondBox.forEach((el) => {
-  // ეშვება დაკვრის ფუნქცია
-  play(el)
-});
-
-
-thirdBox.forEach((el) => {
-  // ეშვება დაკვრის ფუნქცია
-  play(el)
-});
+cards.forEach(card => card.addEventListener('click', flipCard));
 
 
-checkBoxes.forEach(elm => {
-    elm.addEventListener('click', (evt)=>{
-      // შლის active კლასს თუ თამაშის პირველი ეტაპი დასტულებულია
-      checkBoxes.forEach(e => {
-        if(!firstStage) {
-          e.classList.remove('active')
-        }
-      })
-      // შლის active კლასს და შლისს data-id მასივიდან
-      if(evt.target.classList.contains('active')){
-        let index = check.indexOf(evt.target.dataset.id)
-        check.splice(index, 1)
-        evt.target.classList.remove('active')
 
-
-        // ამატებს active კლასს და ინახავს data-id მასივში
-      }else if(!evt.target.classList.contains('active')){
-        evt.target.classList.add('active')
-        check.push(evt.target.dataset.id)
-        if(!firstStage){
-          secondCheck = evt.target.dataset.id
-        }
-      }
+document.getElementById('resetBtn').addEventListener("click", () => {
+    cards.forEach(card => {
+        card.addEventListener('click', flipCard)
+        resetBoard();
+        card.classList.remove('flip')
     })
 })
 
-//უკრავს ხმას სურათებზე დაჭერისას
-function play(el) {
-    el.addEventListener("click", (evt) => {
-      // დაუკრას თუ "მოუსმინეს" დააჭირა
-      if(isActive){
-      let id = evt.target.dataset.id;
-      let voice = 'data-voice-' + id
-      handleLoadstop();
-      createjs.Sound.registerSound({
-        src: `${evt.target.getAttribute(voice)}`,
-        id: "sound",
-      });
-      handleLoadComplete();
-      }
-    });
+
+/// ხმის გაშვება
+createjs.Sound.on("fileload", handleLoadComplete);
+createjs.Sound.alternateExtensions = ["wav"];
+function handleLoadComplete(event) {
+    createjs.Sound.play("sound");
 }
-
-
-document.getElementById('completedGame').addEventListener('click', () => {
-  // გადადის მეორე დონეზე თუ მონიშნულია 1 და 3 (ჩნდება 2 აბზაცი)
-  if ( check.length === 2 && check.includes('1') && check.includes('3')){
-    secondLine.style.display = 'block'
-    document.getElementById(`box-${check[0]}`).classList.add('true')
-    document.getElementById(`box-${check[1]}`).classList.add('true')
-    setTimeout(()=>{
-      firstStage = false;
-      checkBoxes.forEach(el => {
-        el.classList.remove('active')
-      })
-      document.getElementById(`box-${check[0]}`).classList.remove('true')
-      document.getElementById(`box-${check[1]}`).classList.remove('true')
-    }, 2000)
-  }else{
-    let wrong = check.indexOf('2')
-    document.getElementById(`box-${check[wrong]}`).classList.add('false')
-    console.log(a)
-  }
-  // ასრულებს თუ მოეორე დონეზე მონიშნულია 1
-  if(secondCheck === '1'){
-    location.href = "./game-success-21.html";
-  }
-})
+function handleLoadstop(event) {
+    createjs.Sound.stop("sound");
+}
