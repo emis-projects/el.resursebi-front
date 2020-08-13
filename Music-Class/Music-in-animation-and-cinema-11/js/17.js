d37ff0a9f117a8b3e6e445e13ea98e28e77915ac
jemali.tediashvili10b@geolab.edu.ge <43757639+front-end-jeko@users.noreply.github.com>
2020-08-13 20:31:37 +0400

    added music 11 games

---
+++ Music-Class/Music-in-animation-and-cinema-11/js/17.js
@@ -0,0 +1,139 @@
+createjs.Sound.on("fileload", handleLoadComplete);
+createjs.Sound.alternateExtensions = ["wav"];
+
+function handleLoadComplete(event) {
+    createjs.Sound.play("sound");
+}
+
+function handleLoadstop(event) {
+    createjs.Sound.stop("sound");
+}
+
+
+function game() {
+    this.answersArray = '';
+    this.error1 = true;
+    this.error2 = true;
+    this.userAnswer1 = "1-3";
+    this.userAnswer2 = "2-2";
+
+
+
+    let listenBtn = document.querySelectorAll('.listen--btn');
+    let items = document.querySelectorAll('.DragGame—childs1');
+    let items2 = document.querySelectorAll('.DragGame—childs2');
+    let correctAnswer = document.getElementById('correctAnswer');
+    const completeBtn = document.getElementById('completedGame');
+    const resetBtn = document.getElementById('resetBtn');
+
+
+    // listeners
+    completeBtn.addEventListener('click', () => this.completGame());
+    resetBtn.addEventListener('click', () => this.init());
+
+
+
+    $('.listen--btn').click((e) => {
+        handleLoadstop()
+        createjs.Sound.registerSound({src:`${e.target.getAttribute('data-voice')}`, id:"sound"});
+        handleLoadComplete()
+    })
+
+
+    document.querySelector('.DragGame—childs1').querySelectorAll('.class_music_3-check_box_item').forEach(w => {
+        w.addEventListener('click', (e) => {
+            if(this.error1 || this.error2) {
+                $('.DragGame—childs1 .class_music_3-check_box_item').removeClass('selected')
+                $('.DragGame—childs1 .class_music_3-check_box_item').removeClass('error')
+    
+                e.target.classList.add('selected')
+    
+                if(e.target.getAttribute('data-index') == this.userAnswer1){
+                    this.error1 = false;
+                    e.target.classList.add('correct--game')
+    
+                } else {
+                    this.error1 = true;
+                    e.target.classList.remove('correct--game')
+                }
+            }
+        })
+    })
+
+
+    document.querySelector('.DragGame—childs2').querySelectorAll('.class_music_3-check_box_item').forEach(w => {
+        w.addEventListener('click', (e) => {
+            if(this.error1 || this.error2) {
+                $('.DragGame—childs2 .class_music_3-check_box_item').removeClass('selected')
+                $('.DragGame—childs2 .class_music_3-check_box_item').removeClass('error')
+    
+                e.target.classList.add('selected')
+    
+                if(e.target.getAttribute('data-index') == this.userAnswer2){
+                    this.error2 = false;
+                    e.target.classList.add('correct--game')
+    
+                } else {
+                    this.error2 = true;
+                    e.target.classList.remove('correct--game')
+                }
+            }
+        })
+    })
+
+
+
+    this.checkGameAnswers = () => {
+        if(this.error1) {
+            $('.DragGame—childs1 .class_music_3-check_box_item').removeClass('selected')
+            $('.DragGame—childs1 .class_music_3-check_box_item').removeClass('success')
+            $('.DragGame—childs1 .class_music_3-check_box_item').addClass('error')
+            
+        } else {
+            $('.DragGame—childs1 .correct--game').removeClass('selected')
+            $('.DragGame—childs1 .correct--game').removeClass('error')
+            $('.DragGame—childs1 .correct--game').addClass('success')
+        }
+
+        if(this.error2) {
+            $('.DragGame—childs2 .class_music_3-check_box_item').removeClass('selected')
+            $('.DragGame—childs2 .class_music_3-check_box_item').removeClass('success')
+            $('.DragGame—childs2 .class_music_3-check_box_item').addClass('error')
+
+        } else {
+            $('.DragGame—childs2 .correct--game').removeClass('selected')
+            $('.DragGame—childs2 .correct--game').removeClass('error')
+            $('.DragGame—childs2 .correct--game').addClass('success')
+        }
+
+
+        if(!this.error1 && !this.error2){
+            location.href = "game-success-16.html"
+        }
+    }
+
+
+    this.init = () => {
+        $('.class_music_3-check_box_item').removeClass('selected');
+        $('.class_music_3-check_box_item').removeClass('error');
+        $('.class_music_3-check_box_item').removeClass('success');
+        $('.class_music_3-check_box_item').removeClass('correct--game');
+
+        this.error1 = true;
+        this.error2 = true;
+
+        // stop voice 
+        createjs.Sound.stop("sound");
+    }
+    
+
+    this.completGame = () => {
+        this.checkGameAnswers()
+        
+        // stop voice 
+        createjs.Sound.stop("sound");
+    }
+}
+
+
+const Game = new game();
