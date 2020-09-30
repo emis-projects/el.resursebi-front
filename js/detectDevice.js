6aa7094325facd8ffa4aae9be7b177df8981fb5f
jemali.tediashvili10b@geolab.edu.ge <43757639+front-end-jeko@users.noreply.github.com>
2020-09-30 17:20:55 +0400

    update responsive

--- js/detectDevice.js
+++ js/detectDevice.js
@@ -1,102 +1,44 @@
- 
+// is this an IPad ?
+var isiPad = (navigator.userAgent.match(/iPad/i) != null);
 
-function renderNextPage() {
-    let width = window.innerWidth;
-    let height = window.innerHeight;
+// is this an iPhone ?
+var isiPhone = (navigator.userAgent.match(/iPhone/i) != null);
 
-
-    if(width < 768 || height < 768){
-        console.log('1')
-        localStorage.setItem('renderNextPage', JSON.stringify(false))
-        localStorage.setItem('index', JSON.stringify('1'))
-        
-    } else if(width >= 768 && width <= 1000 && height >= 1000) {
-        console.log('2')
-        localStorage.setItem('renderNextPage', JSON.stringify(true))
-        localStorage.setItem('index', JSON.stringify('2'))
-        
-    } else if(width >= 1000 && height >= 768) {
-        console.log('3')
-        localStorage.setItem('renderNextPage', JSON.stringify(true))
-        localStorage.setItem('index', JSON.stringify('3'))
-
-    } else {
-        console.log('4')
-        localStorage.setItem('renderNextPage', JSON.stringify(false))
-        localStorage.setItem('index', JSON.stringify('4'))
-    }
-};
- 
+// is this an windows ?
+var isWindows = (navigator.userAgent.match(/Windows/i) != null);
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    renderNextPage()
-
-    let index = JSON.parse(localStorage.getItem('index'));
-    let NextPage = JSON.parse(localStorage.getItem('renderNextPage'));
-    let hasChanged = JSON.parse(localStorage.getItem('hasChanged'));
-
-
-    if(index == '1' && NextPage == false) {
-        document.querySelector('body').setAttribute('style', 'display: none')
-        redirectUser(false, '/main-page/portrait.html', `${location.origin}/el.resursebi-front/main-page/portrait.html`)
-        
-    } else if (index == '2' && NextPage == true) {
-        document.querySelector('body').setAttribute('style', 'display: none')
-        redirectUser(false, '/main-page/landscape.html', `${location.origin}/el.resursebi-front/main-page/landscape.html`)
-
-    } else if (index == '3' && NextPage == true && !hasChanged) {
-        localStorage.setItem('hasChanged', JSON.stringify(true))
-        document.querySelector('body').setAttribute('style', 'display: none')
-        redirectUser(false, '/', `${location.origin}/el.resursebi-front`)
-    }
-})
-
-
-window.addEventListener('orientationchange', function(e) {
-    renderNextPage()
-});
-
-window.addEventListener('resize', function(e) {
-    renderNextPage()
+if (isiPhone) {
+    document.querySelector('body').setAttribute('style', 'display: none')
+    redirectUser('/main-page/portrait.html', `${location.origin}/el.resursebi-front/main-page/portrait.html`)
     
-    let index = JSON.parse(localStorage.getItem('index'));
-    let NextPage = JSON.parse(localStorage.getItem('renderNextPage'));
-    let hasChanged = JSON.parse(localStorage.getItem('hasChanged'));
-
-    if(index == '2' && NextPage == true) {
-        document.querySelector('body').setAttribute('style', 'display: none')
-        redirectUser(false, '/main-page/landscape.html', `${location.origin}/el.resursebi-front/main-page/landscape.html`)
+} else if(isiPad) {
+    if(window.innerWidth > 1000 && window.innerHeight > 768 && !JSON.parse(localStorage.getItem('refresh')) == true) {
+        localStorage.setItem('refresh', JSON.stringify(true))        
+        redirectUser('/', '/el.resursebi-front')  
         
+    } else if(!JSON.parse(localStorage.getItem('refresh')) == true) {
+        localStorage.setItem('refresh', JSON.stringify(true))        
+        redirectUser('/main-page/landscape.html', `${location.origin}/el.resursebi-front/main-page/landscape.html`)
+    }
+
+} else {
+    if(!JSON.parse(localStorage.getItem('refresh')) == true) {
+        localStorage.setItem('refresh', JSON.stringify(true))        
+        redirectUser('/', '/el.resursebi-front')  
     } 
+}
 
-});
 
 
-function isMobile() {
-    let width = window.innerWidth;
-    let height = window.innerHeight;
+function redirectUser(schoolLink, githubLink) {
+    if(location.host == "school.emis.ge") {
+        location.href = schoolLink
 
-    if(width < 1000 || height < 768 ){
-        return false
+    } else if(location.host == "ananoaspanidze.github.io") {
+        location.href = githubLink
 
     } else {
-        return true
-    }
-};
-
-
-function redirectUser(ismobile, schoolLink, githubLink) {
-    if(ismobile == false) {
-        if(location.host == "school.emis.ge") {
-            location.href = schoolLink
-    
-        } else if(location.host == "ananoaspanidze.github.io") {
-            location.href = githubLink
-    
-        } else {
-            location.href = schoolLink
-            
-        }
+        location.href = schoolLink
     }
 }
