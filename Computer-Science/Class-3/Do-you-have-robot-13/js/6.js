3ec7a93451947d13a908e25563bc798e69b515d6
Jeko <43757639+front-end-jeko@users.noreply.github.com>
2020-12-09 16:51:28 +0400

    update

--- Computer-Science/Class-3/Do-you-have-robot-13/js/6.js
+++ Computer-Science/Class-3/Do-you-have-robot-13/js/6.js
@@ -89,10 +89,13 @@ function musicGames() {
         myArray.forEach(element => {
             if (element.getAttribute('data-place') == element.parentElement.getAttribute('data-place')) {
                 element.parentElement.style.borderColor = "#a1dd6f";
+                element.parentElement.style.borderWidth = "2px";
+
 
             }
             else if (element.parentElement.classList.contains('myDrag')) {
                 element.parentElement.style.borderColor = "#dc6c85";
+                element.parentElement.style.borderWidth = "2px";
             }
 
 
